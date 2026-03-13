f79559ae33b186f5334c6508aa0d2667ae6e5429
aar0nr04 <aar0nr@hotmail.com>
2026-03-12 22:09:57 -0600

    Fix GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-});
-=======
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/testamente/',
 })
->>>>>>> 2a73a14 (Initial Testamente web platform)
